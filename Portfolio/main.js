9d83ad14c01018c6b504e0e7b886a2656af257aa
Himanshu <118209252+psjhimanshu@users.noreply.github.com>
2024-03-02 22:00:40 +0530

    Delete Portfolio directory

--- Portfolio/main.js
+++
@@ -1,8 +0,0 @@
-// var typed=new Typed(".text", {
-//     Strings:["Programmer","Web-Developer","Android-Developer"],
-//     typeSpeed:100,
-//     backSpeed:100,
-//     backDelay:1000,
-//     loop:true
-
-// });
